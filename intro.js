180065e56e92af06e83fd1c75dd7bac4050582d8
eleverything96-bit <eleverything96@gmail.com>
2026-07-11 18:20:31 +0100

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "precious";
+const age = 20;
+const favoriteLanguage = "JavaScript";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favorite Programming Language: ${favoriteLanguage}`);
